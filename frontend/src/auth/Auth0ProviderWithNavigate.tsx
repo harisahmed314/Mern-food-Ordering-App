6af02a4d17897245c0241b2d4ac4137ae4661625
harisahmed314 <harisahmed314@gmail.com>
2024-08-07 10:31:01 +0500

    update auth

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -10,9 +10,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
-  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri || !audience) {
+  if (!domain || !clientId || !redirectUri) {
     throw new Error("unable to initialise auth");
   }
 
