2fe74641578f82cb9c14494b9826a1cefd2e4f72
harisahmed314 <harisahmed314@gmail.com>
2024-08-07 11:07:19 +0500

    udate mobile nav

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,25 +1,51 @@
-import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from './ui/sheet';
-import { Menu } from 'lucide-react';
-import { Separator } from './ui/separator';
-import { Button } from './ui/button';
+import { CircleUserRound, Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobilenavLink from "./MobilenavLink";
 
 const MobileNav = () => {
-    return (
-        <Sheet>
-            <SheetTrigger>
-                <Menu className="text-orange-500" />
-            </SheetTrigger>
-            <SheetContent className='space-y-3'>
-                <SheetTitle>
-                    <span>welcome to mernsEats.com</span>
-                </SheetTitle>
-                <Separator />
-                <SheetDescription className='flex'>
-                    <Button className='flex-1 font-bold bg-orange-500'>LOG IN</Button>
-                </SheetDescription>
-            </SheetContent>
-        </Sheet>
-    );
+  const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-orange-500" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          {isAuthenticated ? (
+            <span className="flex items-center font-bold gap-2">
+              <CircleUserRound className="text-orange-500" />
+              {user?.email}
+            </span>
+          ) : (
+            <span> Welcome to MernEats.com!</span>
+          )}
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex flex-col gap-4">
+          {isAuthenticated ? (
+            <MobilenavLink />
+          ) : (
+            <Button
+              onClick={() => loginWithRedirect()}
+              className="flex-1 font-bold bg-orange-500"
+            >
+              Log In
+            </Button>
+          )}
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
