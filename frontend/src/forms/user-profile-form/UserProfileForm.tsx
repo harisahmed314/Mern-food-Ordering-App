58aa6d2adbb1142ec4133950883e6c8a51d6bdd9
harisahmed314 <harisahmed314@gmail.com>
2024-08-17 09:24:23 +0500

    changes and addition in user profile foam

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -11,13 +11,11 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import LoadingButton from "@/components/LoadingButton";
 import { Button } from "@/components/ui/button";
-import { useEffect } from "react";
 
 const formSchema = z.object({
-  email: z.string().optional(),
-  name: z.string().min(1, "name is required"),
+  email: z.string().email("Invalid email address").optional(),
+  name: z.string().min(1, "Name is required"),
   addressLine1: z.string().min(1, "Address Line 1 is required"),
   city: z.string().min(1, "City is required"),
   country: z.string().min(1, "Country is required"),
@@ -28,17 +26,13 @@ export type UserFormData = z.infer<typeof formSchema>;
 type Props = {
   onSave: (userProfileData: UserFormData) => void;
   isLoading: boolean;
-  
 };
 
-const UserProfileForm = ({
-  onSave,
-  isLoading}: Props) => {
+const UserProfileForm = ({ onSave, isLoading }: Props) => {
   const form = useForm<UserFormData>({
     resolver: zodResolver(formSchema),
   });
 
- 
   return (
     <Form {...form}>
       <form
@@ -46,11 +40,12 @@ const UserProfileForm = ({
         className="space-y-4 bg-gray-50 rounded-lg md:p-10"
       >
         <div>
-          <h2 className="text-2xl font-bold">user profile page</h2>
+          <h2 className="text-2xl font-bold">User Profile</h2>
           <FormDescription>
-            View and change your profile information here
+            View and change your profile information here.
           </FormDescription>
         </div>
+
         <FormField
           control={form.control}
           name="email"
@@ -119,7 +114,9 @@ const UserProfileForm = ({
             )}
           />
         </div>
-        
+        <Button type="submit" className="bg-orange-500" disabled={isLoading}>
+          {isLoading ? "Saving..." : "Save"}
+        </Button>
       </form>
     </Form>
   );
