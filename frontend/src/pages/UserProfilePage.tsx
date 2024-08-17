6fc37c6138875ceb31d92e038ac36cffbe9c4b44
harisahmed314 <harisahmed314@gmail.com>
2024-08-17 10:06:00 +0500

    settle get currnt user

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,10 +1,19 @@
-import { useUpdateMyUser } from '@/api/MyUserapi'
+import { useGetMyUser, useUpdateMyUser } from '@/api/MyUserapi'
 import UserProfileForm from '@/forms/user-profile-form/UserProfileForm'
 
 const UserProfilePage = () => {
-    const{updateUser,isLoading}=useUpdateMyUser()
+    const {currentUser,isLoading:isGetLoading}=useGetMyUser()
+    const{updateUser,isLoading:isUpdateLoading}=useUpdateMyUser()
+    if (isGetLoading){
+        return <span>loading...</span>
+
+    }
+
+    if(!currentUser){
+        return <span>unable to load user profile</span>
+    }
     return (
-        <UserProfileForm onSave={updateUser} isLoading={isLoading}/>
+        <UserProfileForm onSave={updateUser} isLoading={isUpdateLoading}/>
 )
 }
 
